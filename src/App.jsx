c9754177e96d44d29dd719c35b2c0d337470736a
jubayer <jubayerahmed.dev@gmail.com>
2025-10-06 12:53:24 +0600

    Setup error boundary: using react-error-boundary npm library

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Suspense, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 import Comments from "./Comments";
 import SelectPost from "./SelectPost";
 
@@ -7,15 +8,18 @@ function App() {
 
   return (
     <>
-      <Suspense fallback={<h2>Loading Post select...</h2>}>
-        <SelectPost onChange={(postId) => setSelectedPostId(postId)} />
-      </Suspense>
+      <ErrorBoundary fallback={<h2>Error occur in post selector page</h2>}>
+        <Suspense fallback={<h2>Loading Post select...</h2>}>
+          <SelectPost onChange={(postId) => setSelectedPostId(postId)} />
+        </Suspense>
+      </ErrorBoundary>
 
       {selectedPostId && (
-        <Suspense fallback={<h2>Loading comments...</h2>}>
-          {" "}
-          <Comments postId={selectedPostId} />
-        </Suspense>
+        <ErrorBoundary fallback={<h2>Error occur which rendering comments</h2>}>
+          <Suspense fallback={<h2>Loading comments...</h2>}>
+            <Comments postId={selectedPostId} />
+          </Suspense>
+        </ErrorBoundary>
       )}
     </>
   );
